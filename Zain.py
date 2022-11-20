b93902b420e3dfdb06c77948bd31bd6369710c6e
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-20 21:43:54 +0500

    Zain.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    pip install requests
    pip install mechanize
    pip install bs4
    rm -rf Zain
    git clone --depth=1 https://github.com/MeerHADI-khan/HADI-Zain.git
    cd HADI-Zain
    python Zain.py

---
+++ Zain.py
@@ -0,0 +1,32 @@
+# MEER-HADI
+PAID TOOLS
+<b></b> </br> <br>[![Github](https://img.shields.io/badge/Github-Mr.AKING-dimgray?style=flat-square&logo=github)](https://github.com/AKING110)<br> [![Facebook](https://img.shields.io/badge/Facebook-AKING-blue?style=flat-square&logo=facebook)](https://www.facebook.com/MR.AKING.07)<br> [![Whatsapp](https://img.shields.io/badge/Whatsapp-AKING-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
+
+
+
+<h1 align="center"> [MR.HADI-Zain]</h1>
+
+<h2 align="center">  FB CLONING TOOL </h2>
+
+
+## <b>installation</b>
+
+🔰 _CLONE FULL OK IDZ_
+
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install git`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install bs4`
+- `rm -rf Zain`
+- `git clone --https://github.com/MeerHADI-khan/HADI-Zain.git`
+- `cd HADI-Zain`
+- `python Zain.py`
+
+
+
+ ___This Tools is Paid___</br>
+ [![Whatsapp](https://img.shields.io/badge/Whatsapp-AKING-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
